97f7742c90a075040f31b6b1c087e8e274ccc142
AnkitKumarVeersa <ankit.kumar@veersatech.com>
2023-02-21 01:03:02 +0530

    Add config for email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,20 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    user_name:      'ak974113@gmail.com',
+    password:       'Ankit@2001',
+    domain:         'localhost:3000',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
